b869e5114132405aae244b264acddce26caacf8a
Oluwaseun Peter <adepeter26@gmail.com>
2020-03-14 14:49:53 +0100

    Fixed thread_rule.py

--- flyapps/threads/models/thread_report.py
+++ flyapps/threads/models/thread_report.py
@@ -15,7 +15,6 @@ class ThreadRule(models.Model):
 
 class ThreadReport(models.Model):
     thread = models.ForeignKey(Thread, on_delete=models.CASCADE, related_name='thread_reports')
-    reported_by = models.ManyToManyField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
-                                         related_name='thread_reports')
-    rules_broken = models.ManyToManyField(ThreadRule, on_delete=models.CASCADE, related_name='thread_reports')
+    reported_by = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='thread_reports')
+    rules_broken = models.ManyToManyField(ThreadRule, related_name='thread_reports')
     reported_on = models.DateTimeField(default=timezone.now)
