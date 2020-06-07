f1182a00e2f9337da2ced7c613b59673d62ce535
adepeter <adepeter26@gmail.com>
2020-06-07 12:01:36 +0100

    Added Ban model to users app

--- flyapps/users/models/__init__.py
+++ flyapps/users/models/__init__.py
@@ -1,2 +1,3 @@
+from .ban import Ban
 from .role import Role, Roleship
 from .user import User
---
+++ flyapps/users/models/ban.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+
+class Ban(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL
+    )
+    banned_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        limit_choices_to={'is_superuser': True}
+    )
+    message = models.TextField(verbose_name=_('Ban message'))
+    until = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user.username} has been banned'
