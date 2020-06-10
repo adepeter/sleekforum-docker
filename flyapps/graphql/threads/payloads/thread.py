98e5c072b982b4c23d1ccf510da271658d038fa4
adepeter <adepeter26@gmail.com>
2020-06-10 15:20:39 +0100

    GRAPHQL for thread section completed

--- flyapps/graphql/threads/payloads/thread.py
+++ flyapps/graphql/threads/payloads/thread.py
@@ -7,17 +7,30 @@ import graphene
 
 from ....categories.models import Category
 
-from ...utils import DictifyInput
+from ....threads.models.thread import Thread
 
-from ..inputs.thread import ThreadCreateInput
+from ...utils import DictifyNestedInput as DictifyInput
+
+from ..inputs.thread import (
+    ThreadCreateInput,
+    ThreadDeleteInput,
+    ThreadEditInput,
+)
 from ..types.thread.thread import ThreadType
 
 
 class ThreadCreatePayload(graphene.Mutation):
-    thread = graphene.Field(ThreadType)
+    """Payload for creating new thread"""
+    thread = graphene.Field(
+        ThreadType,
+        description=_('Newly created thread type')
+    )
 
     class Arguments:
-        input = ThreadCreateInput()
+        input = ThreadCreateInput(
+            description=_('New thread inputs'),
+            required=True
+        )
 
     def mutate(self, info, input):
         user = info.context.user
@@ -28,9 +41,87 @@ class ThreadCreatePayload(graphene.Mutation):
             serialize_thread.update({
                 'tags': [slugify(x) for x in serialize_thread['tags']]
             })
-        print(serialize_thread)
-        # category = get_object_or_404(Category, **serialize_category)
-        # if not user.is_authenticated:
-        #     raise PermissionDenied(_('You cannot perform this operation'))
-        # thread = category.threads.create(starter=user, **serialize_thread)
-        # return ThreadCreatePayload(thread=thread)
+        category = get_object_or_404(Category, **serialize_category)
+        if not user.is_authenticated:
+            raise PermissionDenied(_('You cannot perform this operation'))
+        thread = category.threads.create(starter=user, **serialize_thread)
+        return ThreadCreatePayload(thread=thread)
+
+
+class ThreadEditPayload(graphene.Mutation):
+    """Payload for updating exiting thread"""
+    thread = graphene.Field(
+        ThreadType,
+        description=_('Edited thread'),
+        required=True
+    )
+
+    class Arguments:
+        input = ThreadEditInput(
+            description=_('thread type to edit'),
+            required=True,
+        )
+
+    def mutate(self, info, input):
+        input = DictifyInput(input)
+        thread = input.get_fields_or_field('thread')
+        patch = input.get_fields_or_field('patch')
+        category_slug = thread.pop('category_slug')
+        obj = get_object_or_404(
+            Thread,
+            category__slug__iexact=category_slug,
+            **thread
+        )
+        if patch is not None:
+            if 'category' in patch:
+                category = get_object_or_404(Category, **patch['category'])
+            for k, v in patch['thread'].items():
+                setattr(obj, k, v)
+            obj.category = category
+            obj.slug = slugify(obj.slug)
+            obj.save()
+        return ThreadEditPayload(thread=obj)
+
+class ThreadDeletePayload(graphene.Mutation):
+    """Payload for thread deletion"""
+    threads = graphene.List(
+        ThreadType,
+        description=_('List of threads in same category after deletion')
+    )
+    thread = graphene.Field(
+        ThreadType,
+        description=_('Thread to delete')
+    )
+    is_deleted = graphene.Boolean(
+        description=_('Confirmation whether thread is deleted'),
+        required=True
+    )
+    
+    class Arguments:
+        input = ThreadDeleteInput(
+            description=_('thread type to delete'),
+            required=True
+        )
+    
+    def mutate(self, info, input):
+        thread = get_object_or_404(
+            Thread,
+            category__slug__iexact=input.category_slug,
+            id=input.id,
+            slug__iexact=input.slug
+        )
+        threads = Thread.objects.filter(
+            category__slug=input.category_slug
+        )
+        if not info.context.user.is_superuser:
+            return ThreadDeletePayload(
+                threads=threads,
+                thread=thread,
+                is_deleted=False
+            )
+        else:
+            thread.delete()
+            return ThreadDeletePayload(
+                threads=threads,
+                is_deleted=True
+            )
--- flyapps/graphql/utils.py
+++ flyapps/graphql/utils.py
@@ -1,5 +1,11 @@
-class DictifyInput:
-    """Serialize an input and convert to dictionary"""
+from typing import Dict
+
+
+class DictifyNestedInput:
+    """Util class that serializes nested inputs and convert to dictionary
+    :param input_arg: an input instance
+    :returns None:
+    """
     def __init__(self, input_arg):
         """Initialize class by passing the input to serialize as dict"""
         self.__serialize_input(input_arg)
@@ -9,7 +15,7 @@ class DictifyInput:
         for k, v in obj.items():
             setattr(self, k, v)
 
-    def get_fields_or_field(self, instance, field=None):
+    def get_fields_or_field(self, instance, field=None) -> Dict or str:
         """Returns a dictionary of fields or get specific field"""
         if hasattr(self, instance):
             instance = getattr(self, instance, None)
@@ -25,6 +31,6 @@ class DictifyInput:
             raise AttributeError('%s is not a valid input' % instance)
 
     @staticmethod
-    def _dictify(param):
+    def _dictify(param: Dict) -> Dict:
         """Converts supplied param to dict"""
         return {field: value for field, value in param.items()}
